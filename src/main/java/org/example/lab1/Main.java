3c7e204b7700d2ef49a3387326411e6e1ce6e035
Alfred <alfred.reintz@icloud.com>
2026-01-30 15:18:45 +0100

    fixed last things

--- src/main/java/org/example/lab1/Main.java
+++ src/main/java/org/example/lab1/Main.java
@@ -5,14 +5,7 @@ import java.awt.*;
 public class Main {
     static void main(String[] args) {
         Car c = new Saab95(Color.black);
-        c.gas(1);
-        c.brake(1);
-        c.incrementSpeed(100);
-        c.gas(1);
-        c.startEngine();
-        c.brake(1);
-        c.gas(1);
-        c.incrementSpeed(100);
-        System.out.println(c.getCurrentSpeed());
+        c = new Volvo240(Color.WHITE);
+
     }
 }
