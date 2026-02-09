9ccd7d78a682e49141dc6aba36ecd90cd034a0a5
Alfred <alfred.reintz@icloud.com>
2026-02-09 16:38:25 +0100

    fixed delegation

--- src/main/java/org/example/lab1/Saab95.java
+++ src/main/java/org/example/lab1/Saab95.java
@@ -12,4 +12,36 @@ public class Saab95 {
     public Car<TurboEngine> getCar() {
         return car;
     }
+
+    public void startEngine() {
+        car.startEngine();
+    }
+
+    public void stopEngine() {
+        car.stopEngine();
+    }
+
+    public void turnRight() {
+        car.turnRight();
+    }
+
+    public void turnLeft() {
+        car.turnLeft();
+    }
+
+    public void move() {
+        car.move();
+    }
+
+    public void gas(double amount) {
+        car.gas(amount);
+    }
+
+    public void brake(double amount) {
+        car.brake(amount);
+    }
+
+    public double getCurrentSpeed() {
+        return car.getCurrentSpeed();
+    }
 }
